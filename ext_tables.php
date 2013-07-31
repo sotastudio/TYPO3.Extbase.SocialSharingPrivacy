2fe05eb857e262e1f761033f1d6817f6e1160736
Andy Hausmann <hi@andy-hausmann.de>
2013-07-31 19:27:28 +0200

    TYPO3 6!

--- ext_tables.php
+++ ext_tables.php
@@ -4,17 +4,17 @@ if (!defined('TYPO3_MODE')) {
 }
 
 // Build extension name vars - used for plugin registration, flexforms and similar
-$extensionName = t3lib_div::underscoredToUpperCamelCase($_EXTKEY);
+$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
 $pluginSignature = strtolower($extensionName) . '_pi1';
 
 
-Tx_Extbase_Utility_Extension::registerPlugin(
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 	$_EXTKEY,
 	'Pi1',
 	'SocialSharingPrivacy'
 );
 
-t3lib_extMgm::addStaticFile(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
 	$_EXTKEY,
 	'Configuration/TypoScript',
 	'SocialSharingPrivacy'
